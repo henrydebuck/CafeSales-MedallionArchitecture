3b3f2e2156932e7bbd3e6f00d928493f2e7095f2
Henry De Buck <59800188+henrydebuck@users.noreply.github.com>
2026-03-24 18:33:11 +0100

    Feedback comments

--- etl.py
+++ etl.py
@@ -4,9 +4,8 @@ import uuid
 from scripts.databaseConnection import connect_to_database
 
 def calcul_total_spent(quantity, priceUnit):
-   if(quantity is not None and priceUnit is not None):
-      return quantity * priceUnit
-   return None
+   # Putting a IF already look at if the value is empty or not
+    return quantity * priceUnit if quantity and priceUnit else None
    
 def save_to_database(schema_name, table_name, dataframe):
    print(f"Saving {schema_name} layer in database...")
@@ -21,7 +20,7 @@ def save_to_database(schema_name, table_name, dataframe):
       print(f"An error occured while saving table {table_name} in schema {schema_name} in database")
 
 if __name__ == '__main__':      
-   DIRTY_COLUMNS = ['ERROR', 'UNKNOWN', float("nan")]
+   DIRTY_COLUMNS = ['ERROR', 'UNKNOWN', float("nan")] # not reliable, use .isna() instead, also this is hardcoding the wrong values. You could use a function to look at the values and search for outliers / dirty elements. Also the name is confusing, this is dirty values not columns.
    DATASET_PATH = 'datasets/dirty_cafe_sales.csv'
    
    # Connection to the database
@@ -76,7 +75,7 @@ if __name__ == '__main__':
    _transform['transaction_date'] = _transform['transaction_date'].replace(DIRTY_COLUMNS, None)
 
    # Change actual ID
-   _transform['id'] = _transform.apply(lambda _: uuid.uuid4(), axis=1)
+   _transform['id'] = _transform.apply(lambda _: uuid.uuid4(), axis=1) # Why changing the ID? Is it a displayable ID for the user or not?
 
    # Transform column in right type
    _transform['quantity'] = pd.to_numeric(_transform['quantity'])
@@ -93,12 +92,21 @@ if __name__ == '__main__':
    
    # Remove rows that contains "None"
    _transform = _transform.dropna().dropna(axis=1)
+   """
+      Dropping in the silver layer is too aggressive, also this way of removing is very destructive. You can lose a lot of data silently.
+      Define critical columns where none is not accepted (for example, 'location' could be empty but not 'Item', 'Quantity', 'Price Per Unit').
+
+      Depending of the philosophy you could either flag bad data and fill some data with means or medians.
+      You could also generate from this layer a Data Quality report that filter out these lines and show it to key users for them to clean them.
+      This kind of decisions should also be documented for better reading and understanding of the report.
+   """
    
    # Add column for year, month and day
    _transform['year'] = pd.DatetimeIndex(_transform['transaction_date']).year
    _transform['month'] = pd.DatetimeIndex(_transform['transaction_date']).month
    _transform['day'] = pd.DatetimeIndex(_transform['transaction_date']).day
-
+   # Why do you add datetime index into silver?
+   
    # Save into the database   
    save_to_database("silver", "cafe_sales", _transform)
    print("Sucessfully process Transform step!")
@@ -108,10 +116,33 @@ if __name__ == '__main__':
    # ------------------------------------
    print("\nLoad processing...")
    _load = _transform.copy()
-   
+
+   """
+      Gold should be ready to use data, not copy/pasted from silver.
+      For example here, you could add the 104-107 lines and split it into a new table named 'dim_date' where you do store all of the dates.
+      This table could looks like that:
+         (dim_date)
+            - id_date
+            - datetime
+            - year
+            - month
+            - day
+            - label_month
+            - label_day
+            - year_month
+            - label_year_month
+            - ... UTC feature for different locations?
+
+      The idea of gold table is ready to use and you should already in it prepare some calculated columns or metrics as much as possible to gain performances in your BI analysis tool.
+      Also use the naming convention dim_ and fact_ (or d_ and f_) for Dimensions and Fact, to help the BI analyst to better read the schema.
+   """
    # Save into the database
    save_to_database("gold", "cafe_sales", _load)
    print("Sucessfully process Load step!")
 
    # Save into csv
    _load.to_csv("gold_layer.csv", index=False)
+
+   """
+      You could split the layers into differents files, that is what we do if we have multiple files and multiple sources, even here this is harder to do and will cost you useless effort because you only have 1 source.
+   """
